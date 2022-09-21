d448735405e431f3b186255ac961757f803b63bc
ali <alagha911@gmail.com>
2022-09-21 14:43:52 +0430

    jsdoc config has been added

--- demo/simple/index.js
+++ demo/simple/index.js
@@ -5,11 +5,12 @@ import * as numerol from '../../lib/index.js';
 /**
  * Old way of using
  */
-numerol.numerolinPersian('1368/03/01');
-numerol.calculateNumerology('1989/05/22');
+// numerol.numerolinPersian('1368/03/01');
+// numerol.calculateNumerology('1989/05/22');
 
 /**
  * New interface:
  */
 const numerolInstance = new numerol.Numeroljs();
-numerolInstance.handle('1368/03/01', 'jalali');
+const a = numerolInstance.handle('1368/03/01', 'jalali');
+console.log(a);
